470b7ce12264694c78254b63f6ae4976dcb7caca
MadhushaPrasad <madushaprasad21@gmail.com>
2021-04-25 19:23:34 +0530

    initial commit

---
+++ includes/common/Header.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <div class="imgDiv">
+        <img src="#" alt="logo">
+    </div>
+    <div class="linkDiv">
+        <ul class="unOrderList">
+            <li>Home</li>
+            <li>Service</li>
+            <li>Finding a job</li>
+            <li>About us</li>
+            <li>Contact Us</li>
+        </ul>
+    </div>
+    <div class="signupDiv">
+        <div class="loginDiv">
+            <button class="btnSignUp">Sign Up</button>
+            <button class="btnLogin">Login In</button>
+        </div>
+    </div>
+</div>
