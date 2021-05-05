3d79c4373c76302df7071ff8736f03b699f656fb
MadhushaPrasad <madushaprasad21@gmail.com>
2021-05-05 19:04:19 +0530

    footer finished

--- includes/common/Footer.php
+++ includes/common/Footer.php
@@ -19,19 +19,33 @@
             <div class="qLinkDivContainer">
                 <h3>Quick Links</h3>
                 <ul>
-                    <li>Home</li>
-                    <li>Service</li>
-                    <li>Finding a job</li>
-                    <li>Contact Us</li>
-                    <li>About Us</li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#">Finding a job</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">About Us</a></li>
                 </ul>
             </div>
         </div>
         <div class="aboutUsDiv">
-
+            <div class="aboutUsDivContainer">
+                <h3>About Us</h3>
+                <p>We are MDC Professionals. Our web site is a method of providing
+                    Employment online. The main purpose of our web site is to provide
+                    a platform for job Seekers and Job Providers to fulfill their needs
+                    efficiently and easily. Our web site has made it easier for the
+                    unemployed and the employed to get exciting opportunities.
+                </p>
+            </div>
         </div>
         <div class="contactUsDiv">
-
+            <div class="contactUsDivContainer">
+                <h3>Contact Us</h3>
+                <p>MDCProfetionals@gmail.com<br>
+                    034-45644545<br>
+                    No:54/5,Malabe,Colombo,Sri Lanka
+                </p>
+            </div>
         </div>
     </div>
 </footer>
