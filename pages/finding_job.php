f6c62cceeaa961aa4d07b5add5b0dc9a39d43b62
Dimuth-J <dimuthdimuth9@gmail.com>
2021-05-12 00:43:27 +0530

    finished finding a job page and login page

---
+++ pages/finding_job.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--    google font css-->
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <!--    custom css-->
+    
+    <link rel="stylesheet" href="../assets/css/finding_job.css">
+    <link rel="stylesheet" href="../assets/css/header.css">
+    <link rel="stylesheet" href="../assets/css/header-image.css">
+    <link rel="stylesheet" href="../assets/css/searchCategory.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
+    
+    <title>MDC | Finding A Jobs</title>
+
+    </head>
+
+    
+
+        <body>
+            <!--header start-->
+            <?php
+            include '../includes/common/header.php';
+            ?>
+            <!--header end-->
+
+            <main>
+                <!--    header images start-->
+                <?php
+                include '../includes/header-image.php';
+                ?>
+                <!--    header images end-->
+
+                <!--    search category start-->
+                <?php
+                include '../includes/searchCategory.php';
+                ?>
+
+        <div class="container">
+
+            <div class="box">
+                <div class="img">
+                    <img src="../assets/images/job-card-image.png" alt="image">
+                </div><br>
+                <h2>Webflow Developer Needed</h2><br>
+                <p> Hello, i am lookimng for a webflow expert to work part time.
+                At first, it will be small edits here & there, but lookong for a
+                long term partnership as more projects come in.</p><br>
+
+                <div class="btn">
+                <a href="https://www.google.com/">SEE MORE</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="img">
+                    <img src="../assets/images/job-card-image.png" alt="image">
+                </div><br>
+                <h2>Webflow Developer Needed</h2><br>
+                <p> Hello, i am lookimng for a webflow expert to work part time.
+                At first, it will be small edits here & there, but lookong for a
+                long term partnership as more projects come in.</p><br>
+
+                <div class="btn">
+                <a href="#@#@#@">SEE MORE</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="img">
+                    <img src="../assets/images/job-card-image.png" alt="image">
+                </div><br>
+                <h2>Webflow Developer Needed</h2><br>
+                <p> Hello, i am lookimng for a webflow expert to work part time.
+                At first, it will be small edits here & there, but lookong for a
+                long term partnership as more projects come in.</p><br>
+
+                <div class="btn">
+                <a href="#@#@#@">SEE MORE</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="img">
+                    <img src="../assets/images/job-card-image.png" alt="image">
+                </div><br>
+                <h2>Webflow Developer Needed</h2><br>
+                <p> Hello, i am lookimng for a webflow expert to work part time.
+                At first, it will be small edits here & there, but lookong for a
+                long term partnership as more projects come in.</p><br>
+
+                <div class="btn">
+                <a href="#@#@#@">SEE MORE</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="img">
+                    <img src="../assets/images/job-card-image.png" alt="image">
+                </div><br>
+                <h2>Webflow Developer Needed</h2><br>
+                <p> Hello, i am lookimng for a webflow expert to work part time.
+                At first, it will be small edits here & there, but lookong for a
+                long term partnership as more projects come in.</p><br>
+
+                <div class="btn">
+                <a href="#@#@#@">SEE MORE</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="img">
+                    <img src="../assets/images/job-card-image.png" alt="image">
+                </div><br>
+                <h2>Webflow Developer Needed</h2><br>
+                <p> Hello, i am lookimng for a webflow expert to work part time.
+                At first, it will be small edits here & there, but lookong for a
+                long term partnership as more projects come in.</p><br>
+
+                <div class="btn">
+                <a href="#@#@#@">SEE MORE</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="img">
+                    <img src="../assets/images/job-card-image.png" alt="image">
+                </div><br>
+                <h2>Webflow Developer Needed</h2><br>
+                <p> Hello, i am lookimng for a webflow expert to work part time.
+                At first, it will be small edits here & there, but lookong for a
+                long term partnership as more projects come in.</p><br>
+
+                <div class="btn">
+                <a href="#@#@#@">SEE MORE</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="img">
+                    <img src="../assets/images/job-card-image.png" alt="image">
+                </div><br>
+                <h2>Webflow Developer Needed</h2><br>
+                <p> Hello, i am lookimng for a webflow expert to work part time.
+                At first, it will be small edits here & there, but lookong for a
+                long term partnership as more projects come in.</p><br>
+
+                <div class="btn">
+                <a href="#@#@#@">SEE MORE</a>
+                </div>
+            </div>
+
+        </div> 
+                 <!--    footer start-->
+                 <?php
+                include '../includes/common/footer.php';
+                ?>
+                <!--    footer end-->
+        </main>
+
+    </body>
+
+
+</html>
