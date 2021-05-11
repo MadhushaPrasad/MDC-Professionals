a4fb91e8b0b44ba0454bf233265806f0bc905f7e
MadhushaPrasad <madushaprasad21@gmail.com>
2021-05-12 02:06:58 +0530

    SignUp Welcome page finished

---
+++ pages/signup-welcome.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--    google font css-->
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <!--    custom css-->
+    <link rel="stylesheet" href="../assets/css/signUp_welcome.css">
+    <title>MDC | Sign Up Welcome</title>
+</head>
+<body>
+<p>Click the button <br> what you are need to be register</p>
+<h1>MDC</h1> <br>
+<h1>professionals</h1>
+<div class="btnGroupDiv">
+    <button>Job Seeker</button>
+    <button>Job Provider</button>
+</div>
+</body>
+</html>
