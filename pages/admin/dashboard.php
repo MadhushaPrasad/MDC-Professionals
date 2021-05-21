42c94305f0cc8b498f8e0fdac0b46b27d2c3325b
MadhushaPrasad <madushaprasad21@gmail.com>
2021-05-21 16:45:54 +0530

    dashboard 70% finished

--- pages/admin/dashboard.php
+++ pages/admin/dashboard.php
@@ -1,1 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MDC | Admin Dashboard</title>
+    <link rel="stylesheet" href="../../assets/css/dashboard.css">
+    <link rel="stylesheet" href="../../assets/css/adminHeader.css">
+    <link rel="stylesheet" href="../../assets/css/adminSlideMenu.css">
+    <link rel="stylesheet" href="../../assets/css/adminFooter.css">
+</head>
+<body>
+<!--header start-->
 <?php
+include '../../includes/common/adminHeader.php';
+?>
+<!--header end-->
+<!--slide menu start-->
+<?php
+include '../../includes/common/adminSlideMenu.php';
+?>
+<!--slide menu end-->
+<main>
+
+
+</main>
+<!--slide menu start-->
+<?php
+include '../../includes/common/adminFooter.php';
+?>
+<!--slide menu end-->
+
+</body>
+</html>
