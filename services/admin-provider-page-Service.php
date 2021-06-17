6646e7bdf257efd960481f9cb2b4a2249c9b6ca4
AbiNissanka <dakshithamail@gmail.com>
2021-06-18 00:11:43 +0530

    iwarai

--- services/admin-provider-page-Service.php
+++ services/admin-provider-page-Service.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-require "../config/DBConnection.php";
 
 if (isset($_POST['submit'])) {
     $targetDir = '..\..\MDC-Professionals\assets\images\upload\\';
@@ -9,16 +7,16 @@ if (isset($_POST['submit'])) {
         if (move_uploaded_file($_FILES["profileImage"]["tmp_name"], $target_file)) {
             $Image = $_FILES["profileImage"]["name"];
             $User_Name = $_POST['User_Name'];
+            $Email = $_POST['Email'];
             $First_Name = $_POST['First_Name'];
             $Last_Name = $_POST['Last_Name'];
             $Address = $_POST['Address'];
             $Phone_Number = $_POST['Phone_Number'];
-            $Email = $_POST['Email'];
             $Password = $_POST['Password'];
             $ComPassword = $_POST['con-Password'];
             $status = "online";
-            if ((!(empty($User_Name || $First_Name || $Last_Name || $Address ||
-                    $Phone_Number || $Email || $Password || $ComPassword || $status))) && ($Password == $ComPassword)) {
+            if ((!(empty($User_Name || $Email || $First_Name || $Last_Name || $Address ||
+                    $Phone_Number  || $Password || $ComPassword || $status))) && ($Password == $ComPassword)) {
 
                 $mysql_insert_data = "INSERT INTO user_r (image,user_name, email, first_name, last_name, address, password,status)
                            VALUES ('$Image','$User_Name', '$Email', '$First_Name', '$Last_Name', '$Address', '$Password','$status')";
@@ -32,27 +30,36 @@ if (isset($_POST['submit'])) {
 
                     if ($newUser > 0 && $connection->affected_rows > 0) {
                         $_SESSION['message'] = "Success";
-                        header("location:/MDC-Professionals/pages/login.php");
+                        //header("location:/MDC-Professionals/pages/login.php");//
+                        echo "Insert success";
                     } else {
                         $_SESSION['message'] = "Fail";
-                        header("location:/MDC-Professionals/pages/provider/providerRegister.php");
+                        //header("location:/MDC-Professionals/pages/provider/providerRegister.php");
+                        echo "not success";
                     }
                 } else {
                     $_SESSION['message'] = "Fail";
-                    header("location:/MDC-Professionals/pages/provider/providerRegister.php");
+                   // header("location:/MDC-Professionals/pages/provider/providerRegister.php");
+                   echo"not";
                 }
             } else {
                 $_SESSION['message'] = "Fail";
-                header("location:/MDC-Professionals/pages/provider/providerRegister.php");
+                //header("location:/MDC-Professionals/pages/provider/providerRegister.php");
+                echo "fail";
             }
 
         } else {
             $_SESSION['message'] = "Fail";
-            header("location:/MDC-Professionals/pages/provider/providerRegister.php");
+            //header("location:/MDC-Professionals/pages/provider/providerRegister.php");
+            
         }
     } else {
         $_SESSION['message'] = "Fail";
         header("location:/MDC-Professionals/pages/provider/providerRegister.php");
     }
 }
+
+	
+
+?>
 
